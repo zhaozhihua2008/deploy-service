455710598722f59156d62dce6511abe4bb5ed8f3
llx <lilianxin101@163.com>
2018-01-16 17:07:12 +0800

    修改了实现类，属性文件和对应测试方法

--- src/main/java/com/boco/deploy/service/DeployServiceImpl.java
+++ src/main/java/com/boco/deploy/service/DeployServiceImpl.java
@@ -1,10 +1,26 @@
 package com.boco.deploy.service;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -29,24 +45,144 @@ public class DeployServiceImpl implements DeployService {
 	
 	@Override
 	public List<PackageData> getAllPackage(String exp) {
-		PackageData data=new PackageData();
-		data.setName("k8s");
-		data.setVersion("v1.7.5");
-		List<PackageData> list=new ArrayList<PackageData>();
-		list.add(data);
+		System.out.println(projectPath);
+		List<PackageData> list = new LinkedList<PackageData>();
+		// 根据路径path读取目录中的文件夹
+		String path = projectPath;
+		File file = new File(path);
+		logger.info("在" + path + "下创建File的对象");
+		if (file.exists()) {
+			File[] files = file.listFiles();
+			int len = files.length;
+			logger.info("该路径下所有文件(夹)有:" + len + "个");
+			// 分别创建names数组和versions数组
+			String[] names = new String[len];
+			String[] versions = new String[len];
+			// 循环路径下所有文件夹
+			for (int i = 0; i < len; i++) {
+				if (files[i].isDirectory()) {
+					// 将文件夹目录名截取
+					String[] dirs = files[i].getName().split("_");
+					if (dirs.length == 2) {
+						for (int j = 0; j < dirs.length; j++) {
+							names[i] = dirs[0];
+							versions[i] = dirs[1];
+						}
+					}
+				}
+				// 循环创建PackageData对象来接收属性值
+				PackageData packageData = new PackageData();
+				packageData.setName(names[i]);
+				packageData.setVersion(versions[i]);
+				logger.info("使用PackageData的对象循环接收属性:第" + (i + 1) + "个");
+				if (names[i] != null && versions[i] != null) {
+					// 无正则时全获取
+					if (exp == null) {
+						list.add(packageData);
+						logger.info("无正则时默认获取全部包");
+					} else {
+						// 正则匹配
+						Pattern pattern = Pattern.compile(exp);
+						Matcher matcher = pattern.matcher(packageData.getId());
+						logger.info("有正则时进行正则匹配");
+						if (matcher.find()) { // 当前为模糊匹配
+							logger.info("当前匹配方式为模糊匹配");
+							list.add(packageData);
+						}
+					}
+				}
+			}
+		}
 		return list;
 	}
 
 	@Override
 	public VariableData getVariable(String packageId) {
-		// TODO Auto-generated method stub
-		return null;
+		VariableData var = new VariableData();
+		Map<String, String> map = new HashMap<String, String>();
+		// 根据path路径创建file对象
+		String path = projectPath + packageId + "/" + Constant.PACKAGE_NAME + "/variable.properties";
+		File file = new File(path);
+		logger.info("在" + path + "下创建File的对象");
+		InputStream input = null;
+		try {
+			// 用property对象接收文件中的属性
+			Properties property = new Properties();
+			input = new BufferedInputStream(new FileInputStream(file));
+			logger.info("从File的对象中生成输入流");
+			property.load(input);
+			logger.info("Properties的对象装载输入流");
+			// 迭代property对象并分别取出属性的key和value放入map中
+			Enumeration<?> en = property.propertyNames();
+			while (en.hasMoreElements()) {
+				String key = (String) en.nextElement();
+				String value = property.getProperty(key);
+				map.put(key, value);
+			}
+			logger.info("依次取出属性键值对");
+			var.setProperties(map);
+			logger.info("取出的属性键值对赋值给VariableData的对象并返回");
+		} catch (IOException e) {
+			logger.error("生成输入流遇到IO异常,请检查文件路径" + path);
+			e.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					logger.error("关闭输入流遇到IO异常");
+					e.printStackTrace();
+				}
+			}
+		}
+		return var;
 	}
 
 	@Override
 	public boolean setVariable(String packageId, VariableData variableData) {
-		System.out.println(variableData);
-		return true;
+		boolean result = false;
+		// 把参数variableData中的属性放入map集合
+		Map<String, String> map = variableData.getProperties();
+		logger.info("把参数variableData中的属性放入map集合");
+		// 根据path路径创建file对象
+		String path = projectPath + packageId + "/" + Constant.PACKAGE_NAME + "/variable.properties";
+		File file = new File(path);
+		logger.info("在" + path + "下创建File的对象");
+		OutputStream output = null;
+		Properties property = new Properties();
+		try {
+			output = new FileOutputStream(file);
+			logger.info("在该路径创建输出流OutputStream的对象");
+			// 遍历读取map集合中的属性并用property对象循环接收
+			Iterator<Entry<String, String>> iter = map.entrySet().iterator();
+			while (iter.hasNext()) {
+				Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
+				String key = entry.getKey();
+				String value = entry.getValue();
+				property.setProperty(key, value);
+			}
+			logger.info("遍历读取map集合中的属性并用Properties的对象循环接收");
+			// 把property对象写入输出流并返回值置为true
+			property.store(output, "Updated properties");
+			result = true;
+			logger.info("接收成功,把Properties的对象写入输出流并将返回值置为true");
+		} catch (FileNotFoundException e) {
+			logger.error("找不到文件,请检查文件路径" + path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.error("写入Properties的对象失败,遇到IO异常");
+			e.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					logger.error("关闭输出流遇到IO异常");
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
 	}
 
 	@Override
@@ -141,18 +277,113 @@ public class DeployServiceImpl implements DeployService {
 	}
 
 	@Override
-	public InstallLogData getInstallLog(String logId, int startIndex,
-			int lineNum) {
-		// TODO Auto-generated method stub
-		return null;
+	public InstallLogData getInstallLog(String logId, int startIndex, int lineNum) {
+		InstallLogData log = new InstallLogData();
+		log.setLogId(logId);
+		// 根据path路径创建file对象
+		String path = Constant.DIR_APP_LOG + logId;
+		File file = new File(path);
+		logger.info("在" + path + "下创建File的对象");
+		BufferedReader reader = null;
+		logger.info("创建BufferedReader对象");
+		StringBuilder builder = new StringBuilder();
+		// 读入文件内容最后放入list集合
+		List<String> list = new ArrayList<String>();
+		logger.info("创建StringBuilder字符串对象和List集合对象");
+		int index = 0;// 文件行数
+		boolean eof = false;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			logger.info("读取File对象");
+			String tempString = null;
+			while ((tempString = reader.readLine()) != null) {
+				list.add("当前第" + (index + 1) + "行: " + tempString);
+				index++;
+			}
+			logger.info("按行读取日志文件,共有" + index + "行");
+			log.setEndIndex(index);
+			if (index == 0) {
+				logger.debug("当前日志为空");
+				eof = true;
+			} else if (startIndex == -1) { // 倒序读取日志
+				logger.info("startIndex为:" + startIndex + ",执行倒序读取日志");
+				// 获取行数小于0行或者超过能获取的最大行数时，只能获取当前最大行数的日志
+				if (lineNum > index || lineNum < 0) {
+					logger.info("获取行数小于0行或者超过能获取的最大行数时，只能获取当前最大行数的日志");
+					System.out.println("当前最多只能获取" + index + "行");
+					logger.debug("当前最多只能获取" + index + "行");
+					lineNum = index;
+					eof = true;
+				}
+				for (int i = index - 1; i >= index - lineNum; i--) {
+					builder.append(list.get(i));
+				}
+				logger.info("循环倒序读取日志");
+			} else if (startIndex >= 0 && startIndex < index) {
+				// 起始为0时从第一行开始
+				if (startIndex == 0) {
+					startIndex++;
+					logger.info("起始为0时从第一行开始");
+				}
+				// 获取行数小于0行或者超过能获取的最大行数时，只能获取当前最大行数的日志
+				if (lineNum >= index - startIndex + 1 || lineNum < 0) {
+					logger.info("获取行数小于0行或者超过能获取的最大行数时，只能获取当前最大行数的日志");
+					System.out.println("当前最多只能获取" + (index - startIndex + 1) + "行");
+					logger.debug("当前最多只能获取" + (index - startIndex + 1) + "行");
+					lineNum = index - startIndex + 1;
+					eof = true;
+				}
+				for (int i = startIndex - 1; i < startIndex + lineNum - 1; i++) {
+					if (i < index) {
+						builder.append(list.get(i));
+					}
+				}
+				logger.info("循环读取日志");
+			 // 起始行数超过最大行时，只能获取从当前最大行数开始的日志
+			} else if (startIndex > index) {
+				System.out.println("当前只有" + index + "行");
+				startIndex = index;
+				eof = true;
+				logger.debug("起始行数:" + startIndex + "超过最大行:" + (index + 1) + "时，只能获取从当前最大行数开始的日志");
+			} else {
+				logger.error("startIndex不能为" + startIndex + "!请输入正确的startIndex值");
+			}
+			log.setData(builder.toString());
+			log.setEof(eof);
+			logger.info("保存读取后的日志文件到InstallLogData对象的Data并返回");
+		} catch (FileNotFoundException e) {
+			logger.error("找不到日志文件,请检查路径:" + path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.error("读取日志文件发生IO异常");
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e1) {
+					logger.error("关闭BufferedReader对象发生IO异常");
+				}
+			}
+		}
+		return log;
 	}
 
+	@SuppressWarnings("unused")
 	private PackageData getPackageById(String packageId) throws Exception{
 		List<PackageData> packages = getAllPackage(packageId);
 		if (packages==null || packages.size()==0) {
 			throw new Exception("can not find package: "+packageId);
 		}
 		return packages.get(0);
+	}
+
+	public String getProjectPath() {
+		return projectPath;
+	}
+
+	public void setProjectPath(String projectPath) {
+		this.projectPath = projectPath;
 	}
 	
 
--- src/test/java/com/boco/deploy/service/DeployServiceImplTest.java
+++ src/test/java/com/boco/deploy/service/DeployServiceImplTest.java
@@ -1,12 +1,9 @@
 package com.boco.deploy.service;
 
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,74 +12,181 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.boco.deploy.data.HostData;
+import com.boco.deploy.data.InstallLogData;
+import com.boco.deploy.data.PackageData;
 import com.boco.deploy.data.VariableData;
 
 public class DeployServiceImplTest {
 	private static final Logger logger = LoggerFactory.getLogger(DeployServiceImplTest.class);
 
-	DeployServiceImpl impl=null;
+	DeployServiceImpl impl = null;
+
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("APP_HOME", "c:/tmp");
-		impl=new DeployServiceImpl();
+		// System.setProperty("APP_HOME", "c:/tmp");
+		System.setProperty("APP_HOME", "E:/tmp");
+		impl = new DeployServiceImpl();
+		impl.setProjectPath("E:/tmp/project");
+		logger.info("setProjectPath(\"E:/tmp/project\")");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
+	// @Test
 	public void testInstallPackage() throws Exception {
-		String packageId="k8s-master_1.7.5.el7";
-		HostData hostData1=new HostData();
+		String packageId = "k8s-master_1.7.5.el7";
+		HostData hostData1 = new HostData();
 		hostData1.setIp("tpd1");
 		hostData1.setUserName("xdpp");
 		hostData1.setPassword("xdpp");
 		hostData1.setSudo(true);
 		hostData1.setSudoPass("xdpp");
-		
-		HostData hostData2=new HostData();
+
+		HostData hostData2 = new HostData();
 		hostData2.setIp("tpd2");
 		hostData2.setUserName("root");
 		hostData2.setPassword("yiyangboco");
-		
-		List<HostData> hostDatas=new ArrayList<HostData>();
+
+		List<HostData> hostDatas = new ArrayList<HostData>();
 		hostDatas.add(hostData1);
 		hostDatas.add(hostData2);
-		
-		Map<String, String> properties=new HashMap<String, String>();
+
+		Map<String, String> properties = new HashMap<String, String>();
 		properties.put("k1", "v1");
-		VariableData variableData=new VariableData();
+		VariableData variableData = new VariableData();
 		variableData.setProperties(properties);
 		impl.installPackage(packageId, hostDatas, variableData);
 	}
 
-	@Test
+	// @Test
 	public void testUninstallPackage() throws Exception {
 
-		String packageId="k8s-master_1.7.5.el7";
-		HostData hostData1=new HostData();
+		String packageId = "k8s-master_1.7.5.el7";
+		HostData hostData1 = new HostData();
 		hostData1.setIp("tpd1");
 		hostData1.setUserName("xdpp");
 		hostData1.setPassword("xdpp");
 		hostData1.setSudo(true);
 		hostData1.setSudoPass("xdpp");
-		
-		HostData hostData2=new HostData();
+
+		HostData hostData2 = new HostData();
 		hostData2.setIp("tpd2");
 		hostData2.setUserName("root");
 		hostData2.setPassword("yiyangboco");
-		
-		List<HostData> hostDatas=new ArrayList<HostData>();
+
+		List<HostData> hostDatas = new ArrayList<HostData>();
 		hostDatas.add(hostData1);
 		hostDatas.add(hostData2);
-		
-		Map<String, String> properties=new HashMap<String, String>();
+
+		Map<String, String> properties = new HashMap<String, String>();
 		properties.put("k1", "v1");
-		VariableData variableData=new VariableData();
+		VariableData variableData = new VariableData();
 		variableData.setProperties(properties);
 		impl.uninstallPackage(packageId, hostDatas, variableData);
-	
+
 	}
 
+	// @Test
+	public void testGetAllPackage() {
+		List<PackageData> allPackage = impl.getAllPackage(null);
+		for (int i = 0; i < allPackage.size(); i++) {
+			System.out.println("输入正则为null时默认匹配全部: 第" + (i + 1) + "个");
+			System.out.println("包名:" + allPackage.get(i).getName());
+			System.out.println("版本号:" + allPackage.get(i).getVersion());
+			System.out.println("包id:" + allPackage.get(i).getId());
+		}
+		System.out.println();
+		List<PackageData> packages1 = impl.getAllPackage("");
+		for (int i = 0; i < packages1.size(); i++) {
+			System.out.println("输入正则为空字符串时默认匹配全部: 第" + (i + 1) + "个");
+			System.out.println("包名:" + allPackage.get(i).getName());
+			System.out.println("版本号:" + allPackage.get(i).getVersion());
+			System.out.println("包id:" + allPackage.get(i).getId());
+		}
+		System.out.println();
+		List<PackageData> packages2 = impl.getAllPackage("test");
+		for (int i = 0; i < packages2.size(); i++) {
+			System.out.println("输入正则为test时的匹配: 第" + (i + 1) + "个");
+			System.out.println("包名:" + allPackage.get(i).getName());
+			System.out.println("版本号:" + allPackage.get(i).getVersion());
+			System.out.println("包id:" + allPackage.get(i).getId());
+		}
+		System.out.println();
+		List<PackageData> packages3 = impl.getAllPackage("1");
+		for (int i = 0; i < packages3.size(); i++) {
+			System.out.println("输入正则为1时的匹配: 第" + (i + 1) + "个");
+			System.out.println("包名:" + allPackage.get(i).getName());
+			System.out.println("版本号:" + allPackage.get(i).getVersion());
+			System.out.println("包id:" + allPackage.get(i).getId());
+		}
+	}
+
+	// @Test
+	public void testGetVariable() {
+		VariableData var1 = impl.getVariable("test_0.0.1");
+		System.out.println("packageId为 test_0.0.1时获取的属性:" + var1.toString());
+		VariableData var2 = impl.getVariable(null);
+		System.out.println("packageId为 null时获取的属性:" + var2.toString());
+	}
+
+	// @Test
+	public void testSetVariable() {
+		VariableData var = new VariableData();
+		Map<String, String> properties = new HashMap<>();
+		properties.put("test1", "test1");
+		properties.put("test2", "test2");
+		properties.put("testkey", "testvalue");
+		var.setProperties(properties);
+		if (impl.setVariable("123_test", var)) {
+			System.out.println(var.toString());
+		}
+	}
+
+	@Test
+	public void testGetInstallLog() {
+		String id1 = "logtest.txt";
+		int start1 = 1;
+		int num1 = 7;
+		InstallLogData log1 = impl.getInstallLog(id1, start1, num1);
+		System.out.println("参数列表:\n当前日志文件id是:" + id1 + ",开始行数startIndex是" + start1 + ",读取行数:" + num1 + "\n文件属性:\n总行数:"
+				+ log1.getEndIndex() + ",日志内容:" + log1.getData() + "\n日志文件是否读取结束:" + log1.isEof() + "\n");
+
+		String id2 = "logtest.txt";
+		int start2 = 0;
+		int num2 = 27;
+		InstallLogData log2 = impl.getInstallLog(id2, start2, num2);
+		System.out.println("参数列表:\n当前日志文件id是:" + id2 + ",开始行数startIndex是" + start2 + ",读取行数:" + num2 + "\n文件属性:\n总行数:"
+				+ log2.getEndIndex() + ",日志内容:" + log2.getData() + "\n日志文件是否读取结束:" + log2.isEof() + "\n");
+
+		String id3 = "logtest.txt";
+		int start3 = -1;
+		int num3 = 17;
+		InstallLogData log3 = impl.getInstallLog(id3, start3, num3);
+		System.out.println("参数列表:\n当前日志文件id是:" + id3 + ",开始行数startIndex是" + start3 + ",读取行数:" + num3 + "\n文件属性:\n总行数:"
+				+ log3.getEndIndex() + ",日志内容:" + log3.getData() + "\n日志文件是否读取结束:" + log3.isEof() + "\n");
+
+		String id4 = "logtest.txt";
+		int start4 = -1;
+		int num4 = 37;
+		InstallLogData log4 = impl.getInstallLog(id4, start4, num4);
+		System.out.println("参数列表:\n当前日志文件id是:" + id4 + ",开始行数startIndex是" + start4 + ",读取行数:" + num4 + "\n文件属性:\n总行数:"
+				+ log4.getEndIndex() + ",日志内容:" + log4.getData() + "\n日志文件是否读取结束:" + log4.isEof() + "\n");
+
+		String id5 = "logtest.txt";
+		int start5 = -20;
+		int num5 = 37;
+		InstallLogData log5 = impl.getInstallLog(id5, start5, num5);
+		System.out.println("参数列表:\n当前日志文件id是:" + id5 + ",开始行数startIndex是" + start5 + ",读取行数:" + num5 + "\n文件属性:\n总行数:"
+				+ log5.getEndIndex() + ",日志内容:" + log5.getData() + "\n日志文件是否读取结束:" + log5.isEof() + "\n");
+
+		String id6 = "none.txt";
+		int start6 = 25;
+		int num6 = 37;
+		InstallLogData log6 = impl.getInstallLog(id6, start6, num6);
+		System.out.println("参数列表:\n当前日志文件id是:" + id6 + ",开始行数startIndex是" + start6 + ",读取行数:" + num6 + "\n文件属性:\n总行数:"
+				+ log6.getEndIndex() + ",日志内容:" + log6.getData() + "\n日志文件是否读取结束:" + log6.isEof() + "\n");
+
+	}
 }
